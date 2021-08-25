6649a9b7ab951adb6ef001f30efe9d8c67557077
Tram Vuong <vuong2@pdx.edu>
2021-08-24 20:35:00 -0700

    javascript file changes

--- contact.js
+++ contact.js
@@ -2,7 +2,6 @@ let form = document.querySelector("form");
 form.addEventListener("submit", handleSubmit);
 
 function handleSubmit(event) {
-
   let name = document.querySelector("#name");
   let email = document.querySelector("#email");
   let message = document.querySelector("#message");
@@ -11,8 +10,6 @@ function handleSubmit(event) {
     console.warn("You must enter all values to submit this form");
   } else {
     console.log("========= Form Submission =========");
-    console.log("\tEmail: ", email.value);
-    console.log("\tMessage: ", message.value);
 
     if (name.value == "") {
       console.log("\tName: no submission");
@@ -20,14 +17,14 @@ function handleSubmit(event) {
       console.log("\tName: ", name.value);
     }
     if (email.value == "") {
-        console.log("\tEmail: no submission");
-      } else {
-        console.log("\tEmail: ", email.value);
+      console.log("\tEmail: no submission");
+    } else {
+      console.log("\tEmail: ", email.value);
     }
     if (message.value == "") {
-        console.log("\tMessage: no submission");
-      } else {
-        console.log("\tMessage: ", message.value);
+      console.log("\tMessage: no submission");
+    } else {
+      console.log("\tMessage: ", message.value);
     }
   }
 }
